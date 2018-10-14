12085808efeaf0a7ef5c3b29f45527ea4b33ec9b
Levi <levi.liukkonen@gmail.com>
2018-10-14 15:18:56 -0500

    Pull port number from environment, else use 8080

--- src/server/index.js
+++ src/server/index.js
@@ -403,7 +403,7 @@ app.get('/api/mapper/:id', function(req, res) {
 })
 
 if(module === require.main) {
-  const server = app.listen(8080, () => {
+  const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
