ad7e91f4a3f70f7f44aa3c05225920dc8fda6634
Levi <levi.liukkonen@gmail.com>
2018-10-15 18:06:12 -0500

    Changed knexfile to work with google cloud environment variables and removed redundant build script

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      socketPath: `cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+      host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
       user: process.env.SQL_USER,
       password: process.env.SQL_PASSWORD,
       database: process.env.SQL_DATABASE,
