63a18d284e39d4c23da8d550d2d710ea90a57ab3
Levi <levi.liukkonen@gmail.com>
2018-12-05 03:05:13 -0600

    Readded webpack build to prestart and fixed migration

--- src/server/db/migrations/20181204195140_change_description_char_length.js
+++ src/server/db/migrations/20181204195140_change_description_char_length.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.raw('alter table maps modify column description varchar(5000)')
+  return knex.schema.raw('alter table maps alter column description type varchar(5000)')
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.raw('alter table maps modify column description varchar(255)')
+  return knex.schema.raw('alter table maps alter column description type varchar(255)')
 };
